e52adaff7dc4154c3f17ad01526beda4958ac607
ZTTY-0111 <1700012880@pku.edu.cn>
2021-05-13 22:30:23 +0800

    tryout

--- pages/orderCenter/myform/myform.js
+++ pages/orderCenter/myform/myform.js
@@ -1,35 +1,74 @@
 const app = getApp();
 Page({
-  data: {
-    
-    "human1": "歪比巴卜歪比巴卜歪比巴卜歪比巴卜歪比巴卜歪比巴卜歪比巴卜",
-    "human2": "帕布罗.迭戈.荷瑟.山迪亚哥.弗朗西斯科.德.保拉.居安.尼波莫切诺.克瑞斯皮尼亚诺.德.罗斯.瑞米迪欧斯.西波瑞亚诺.德.拉.山迪西玛.特立尼达.玛利亚.帕里西奥.克里托.瑞兹.布拉斯科.毕加索",
-    "start": "北京大学45乙504",
-    "target": "北京大学45乙539",
-    "self": "取件码 123456789，大件",
-    "place": "北京大学二教",
-    "message": "轻拿轻放，是玻璃制品",
-    "state": "北大校内闪送王非你莫属，又快又好，666！！打call",
-    "money":3,
+  data:{
+    elements:[
+      {
+        "human1": "歪比巴卜歪比巴卜歪比巴卜歪比巴卜歪比巴卜歪比巴卜歪比巴卜",
+        "human2": "帕布罗.迭戈.荷瑟.山迪亚哥.弗朗西斯科.德.保拉.居安.尼波莫切诺.克瑞斯皮尼亚诺.德.罗斯.瑞米迪欧斯.西波瑞亚诺.德.拉.山迪西玛.特立尼达.玛利亚.帕里西奥.克里托.瑞兹.布拉斯科.毕加索",
+        "start": "北京大学34楼后快递点",
+        "target": "北京大学45乙539",
+        "self": "8号柜 取件码 123456789",
+        "place": "北京大学二教",
+        "message": "大件，轻拿轻放，是玻璃制品",
+        "state": "运送中",
+        "money": "3",
+        "INDEX":"2",
+      },
+      {
+        "human1": "我爱学习",
+        "human2": "帕布罗.迭戈.荷瑟.山迪亚哥.弗朗西斯科.德.保拉.居安.尼波莫切诺.克瑞斯皮尼亚诺.德.罗斯.瑞米迪欧斯.西波瑞亚诺.德.拉.山迪西玛.特立尼达.玛利亚.帕里西奥.克里托.瑞兹.布拉斯科.毕加索",
+        "start": "北京大学34楼后快递点",
+        "target": "北京大学第二教学楼",
+        "self": "近邻宝京东窗口 取件码 123456789",
+        "place": "北京大学五四运动场",
+        "message": "小件，很轻",
+        "state": "运送中",
+        "money": "1",
+        "INDEX":"0",
+      }
+    ],
     show:false,//控制下拉列表的显示隐藏，false隐藏、true显示
     selectData:['1','2','3','4','5'],//下拉列表的数据
-    "index":2,
+    "Index" : 0,
   },
+
     // 点击下拉显示框
     selectTap(){
-    this.setData({
-     show: !this.data.show
-    });
+      this.setData({
+        show: !this.data.show
+      });
     },
     // 点击下拉列表
     optionTap(e){
-    let Index=e.currentTarget.dataset.index;//获取点击的下拉列表的下标
-    this.setData({
-     index:Index,
-     show:!this.data.show
-    });
+      let index=e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+      this.setData({
+        Index:index,
+        show:!this.data.show
+      });
     },
     onLoad: function (options) {
+      var tmpAddressList = this.data.start;
+      wx.setStorage({//存储到本地
+        key:"start",
+        data:tmpAddressList
+      });
+      // console.log("Common Address");
+      const eventChannel = this.getOpenerEventChannel()
+      // eventChannel.emit('acceptDataFromOpenedPage', {data: 'test'});
+      // eventChannel.emit('someEvent', {data: 'test'});
+      // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+      var index = 0;
+      eventChannel.on('acceptDataFromOpenerPage', function(data) {
+        index = data.index;
+      })
+      let choiceidx = this.data.elements[index].INDEX;
+      let Index = this.data.Index;
+      Index = choiceidx;
+      this.setData({
+        idx:index,
+        Index
+      })
+      console.log(this.data.Index);
     },
   showModal(e) {
     this.setData({
@@ -45,6 +84,11 @@ Page({
     console.log(e);
     this.setData({
       index: e.detail.value
+    })
+  },
+  gotoPage:function(e) {
+    wx.navigateTo({
+      url: 'address/address',
     })
   },
   MultiChange(e) {
